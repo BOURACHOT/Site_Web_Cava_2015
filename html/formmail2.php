0d0b730b173e0ba63d3ad57f460fc1bdbff27088
Bojulot <joel.bourachot@orange.fr>
2019-01-29 10:32:10 +0100

    recaptcha =>html\formmail2.php

--- html/formmail2.php
+++ html/formmail2.php
@@ -22,13 +22,43 @@ Sujet du formulaire: ".$_POST['title']."\r\n
 Message: ".$_POST['comments']." \r\n
 ";
 
-$res = mail($TO, $sujet ,$informations, $head);
 
-if (true == $res) {
-Header("Location: http://".$site."/html/formail2_ok.html" );
-} else {
-Header("Location: http://".$site."/html/formail2_pasok.html" );
-}
+
+
+
+
+// debut mise en place recaptcha
+
+    	
+	// Ma clé privée
+	$secret = "6Ld9WI0UAAAAAIhjiWBq6dTSaarhexGZVXGBajhz";
+	// Paramètre renvoyé par le recaptcha
+	$response = $_POST['g-recaptcha-response'];
+	// On récupère l'IP de l'utilisateur
+	$remoteip = $_SERVER['REMOTE_ADDR'];
+	
+	$api_url = "https://www.google.com/recaptcha/api/siteverify?secret=" 
+	    . $secret
+	    . "&response=" . $response
+	    . "&remoteip=" . $remoteip ;
+	
+	$decode = json_decode(file_get_contents($api_url), true);
+	
+	if ($decode['success'] == true) {
+		// C'est un humain
+		$res = mail($TO, $sujet ,$informations, $head);
+		Header("Location: http://".$site."/html/formail2_ok.html" );
+	}
+	
+	else {
+		// C'est un robot ou le code de vérification est incorrecte
+		Header("Location: http://".$site."/html/formail2_pasok.html" );
+	}
+		
+
+
+
+//  fin de mise en place recaptcha
 ?>
 
 
