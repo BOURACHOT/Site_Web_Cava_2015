bb6ff9a6b6306c21091345ff2282ebc1f15b3076
joel bourachot <joel.bourachot@orange.fr>
2020-02-22 18:25:02 +0100

    maj code => formmail2_aurore.html

---
+++ html/formmail2_aurore.php
@@ -0,0 +1,35 @@
+<?php
+/* Remplacer votre_adresse@mail.net par votre adresse mail
+   Remplacer www.votre_domaine.net par votre nom de domaine */
+
+$adresse = "redon.aurore@wanadoo.fr";
+$site = "cava-cebazat.fr";
+
+$TO = $adresse;
+
+$head = "From: ".$adresse."\n";
+$head .= "X-Sender: <".$adresse.">\n";
+$head .= "X-Mailer: PHP\n";
+$head .= "Return-Path: <".$adresse.">\n";
+$head .= "Content-Type: text/plain; charset=iso-8859-1\n";
+
+$sujet = "Formulaire de contact";
+
+$informations = "
+Nom: ".$_POST['realname']." \r\n
+Email du formulaire: ".$_POST['email']." \r\n
+Sujet du formulaire: ".$_POST['title']."\r\n
+Message: ".$_POST['comments']." \r\n
+";
+
+$res = mail($TO, $sujet ,$informations, $head);
+
+if (true == $res) {
+Header("Location: http://".$site."/html/formail2_ok.html" );
+} else {
+Header("Location: http://".$site."/html/formail2_pasok.html" );
+}
+?>
+
+
+
